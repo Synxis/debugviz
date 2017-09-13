7a1da27533510d0c0fafb08553747152b77a5795
Thibault Lescoat <thibault.lescoat@telecom-paristech.org>
2017-09-13 13:23:08 +0200

    Separate code in multiple files

---
+++ graph/source/bbox_collisions.js
@@ -0,0 +1,57 @@
+function bbox_collisions(bbox)
+{
+	var nodes, boxes, strength = 10;
+	function force()
+	{
+		for(var i = 0; i < boxes.length; i++)
+			for(var j = i + 1; j < boxes.length; j++)
+			{
+				var bA = [
+					[nodes[i].x + boxes[i][0][0], nodes[i].y + boxes[i][0][1]],
+					[nodes[i].x + boxes[i][1][0], nodes[i].y + boxes[i][1][1]]
+				];
+				var bB = [
+					[nodes[j].x + boxes[j][0][0], nodes[j].y + boxes[j][0][1]],
+					[nodes[j].x + boxes[j][1][0], nodes[j].y + boxes[j][1][1]]
+				];
+				var cf = collision_force(bA, bB);
+				nodes[i].vx += strength * cf.fA[0];
+				nodes[i].vy += strength * cf.fA[1];
+				nodes[j].vx += strength * cf.fB[0];
+				nodes[j].vy += strength * cf.fB[1];
+			}
+	}
+	function collision_force(bA, bB)
+	{
+		var left   = bB[1][0] - bA[0][0];
+		var right  = bA[1][0] - bB[0][0];
+		var top    = bB[1][1] - bA[0][1];
+		var bottom = bA[1][1] - bB[0][1];
+		var intersectX = (left > 0 && right > 0);
+		var intersectY = (top > 0 && bottom > 0);
+		if(intersectX && intersectY)
+		{
+			var dX = left > right ? right : -left;
+			var dY = top > bottom ? bottom : -top;
+			if(Math.abs(dX) <= Math.abs(dY))
+			{
+				var wA = bA[1][0] - bA[0][0];
+				var wB = bB[1][0] - bB[0][0];
+				return { fA: [-dX / wA, 0], fB: [dX / wB, 0] };
+			}
+			else
+			{
+				var hA = bA[1][1] - bA[0][1];
+				var hB = bB[1][1] - bB[0][1];
+				return { fA: [0, -dY / hA], fB: [0, dY / hB] };
+			}
+		}
+		return { fA: [0, 0], fB: [0, 0] };
+	}
+	force.initialize = function(_)
+	{
+		var i, n = (nodes = _).length; boxes = new Array(n);
+		for(i = 0; i < n; ++i) boxes[i] = bbox(nodes[i], i, nodes);
+	};
+	return force;
+}
---
+++ graph/source/flow_layout.js
@@ -0,0 +1,67 @@
+function flow_layout(graph)
+{
+	var coords = graph.nodes.map(n => { return { name: n.name, x: NaN, y: NaN }; });
+	function successors(node_name)
+	{
+		return [...new Set(graph.connections.filter(c => c.out === node_name).map(c => c.in))];
+	}
+	function predecessors(node_name)
+	{
+		return [...new Set(graph.connections.filter(c => c.in === node_name).map(c => c.out))];
+	}
+	function index(node_name)
+	{
+		return coords.findIndex(n => n.name === node_name);
+	}
+
+	// Ordering on X
+	function horiz_right(node_name)
+	{
+		var i = index(node_name);
+		for(var n of successors(node_name))
+		{
+			var j = index(n);
+			if(n !== node_name && (isNaN(coords[j].x) || true/* !ancestor */))
+			{
+				coords[j].x = Math.max(coords[i].x + 1, isNaN(coords[j].x) ? -Infinity : coords[j].x);
+				horiz_right(n);
+			}
+		}
+	}
+
+	function horiz_left(node_name)
+	{
+		var i = index(node_name);
+		for(var n of predecessors(node_name))
+		{
+			var j = index(n);
+			if(n !== node_name && (isNaN(coords[j].x) || true/* !successor */))
+			{
+				coords[j].x = Math.min(coords[i].x - 1, isNaN(coords[j].x) ? Infinity : coords[j].x);
+				horiz_left(n);
+			}
+		}
+	}
+
+	var roots = graph.nodes.filter(n => predecessors(n.name).length === 0).map(n => n.name);
+	var leaves = graph.nodes.filter(n => successors(n.name).length === 0).map(n => n.name);
+	for(var t = 0; t < roots.length; t++)
+	{
+		coords[index(roots[t])].x = 0;
+		horiz_right(roots[t]);
+	}
+	for(t = 0; t < leaves.length; t++)
+		horiz_left(leaves[t]);
+
+	var all_x = coords.map(n => n.x);
+	var decal = (Math.max(...all_x) - Math.min(...all_x)) / 2;
+	for(var n of coords)
+		n.x -= decal;
+
+	// Ordering on Y
+
+	// We're done
+	coords.index = index;
+	coords.find = function(n) { return coords[coords.index(n)]; };
+	return coords;
+}
